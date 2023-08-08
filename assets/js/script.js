88daec0d63be962df01c9a4d8fb9c0da52f1f9c7
‪Mahmod Adel‬‏ <101188390+Eng-Mahmod-Adel@users.noreply.github.com>
2023-08-08 20:42:54 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,109 @@
+"use strict";
+
+var swiper = new Swiper(".slide_container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // slidesPerGroup: 3,
+  // loop: true,
+  // centerSlide: "true",
+  // grabCursor: "true",
+  fade: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    // clickble: true,
+    // dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+console.log(navbar);
+console.log(overlay);
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
+// const closeNavbar = function () {
+//   navbar.classList.remove("active");
+//   overlay.classList.remove("active");
+// }
+
+// addEventOnElem(navLinks, "click", closeNavbar);
+
+/**
+ * header active when scroll down to 100px
+ */
+
+// const header = document.querySelector("[data-header]");
+// const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+// const activeElem = function () {
+//   if (window.scrollY > 100) {
+//     header.classList.add("active");
+//     backTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     backTopBtn.classList.remove("active");
+//   }
+// }
+
+// addEventOnElem(window, "scroll", activeElem);
